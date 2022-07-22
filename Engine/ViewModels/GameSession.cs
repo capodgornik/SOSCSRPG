f93e154bbbd1cb9da179ecf7fa050697e310b764
capod <capod@LAPTOP-DFP4L1CN>
2022-07-21 20:22:06 -0700

    Connect View to ViewModel

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -7,16 +7,20 @@ using Engine.Models;
 
 namespace Engine.ViewModels
 {
-    internal class GameSession
+    public class GameSession
     {
 
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Celine";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
             CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
         }
     }
 }
