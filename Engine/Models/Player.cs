26b1a35163d59c483b6c5a5100d05a2a5376e9ba
capod <capod@LAPTOP-DFP4L1CN>
2022-07-21 19:18:18 -0700

    Add player class

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    internal class Player
+    {
+        string Name { get; set; }
+        string CharacterClass { get; set; }
+        int HitPoints { get; set; }
+        int ExperiencePoints { get; set; }
+        int Level { get; set; }
+        int Gold { get; set; }
+
+    }
+}
