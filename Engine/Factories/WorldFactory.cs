05e0c9b2a2ef5242987bc27c05c2ed6a66a71b52
capod <capod@LAPTOP-DFP4L1CN>
2022-07-22 11:53:03 -0700

    Add movement buttons

---
+++ Engine/Factories/WorldFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal class WorldFactory
+    {
+        internal World CreateWorld()
+        {
+            World newWorld = new World();
+            newWorld.AddLocation(-2, -1, "Farmer's Field",
+                "There are rows of corn growing here, with giant rats hiding between them.",
+                "/Engine;component/Images/Locations/FarmFields.png");
+            newWorld.AddLocation(-1, -1, "Farmer's House",
+                "This is the house of your neighbor, Farmer Ted.",
+                "/Engine;component/Images/Locations/Farmhouse.png");
+            newWorld.AddLocation(0, -1, "Home",
+                "This is your home",
+                "/Engine;component/Images/Locations/Home.png");
+            newWorld.AddLocation(-1, 0, "Trading Shop",
+                "The shop of Susan, the trader.",
+                "/Engine;component/Images/Locations/Trader.png");
+            newWorld.AddLocation(0, 0, "Town square",
+                "You see a fountain here.",
+                "/Engine;component/Images/Locations/TownSquare.png");
+            newWorld.AddLocation(1, 0, "Town Gate",
+                "There is a gate here, protecting the town from giant spiders.",
+                "/Engine;component/Images/Locations/TownGate.png");
+            newWorld.AddLocation(2, 0, "Spider Forest",
+                "The trees in this forest are covered with spider webs.",
+                "/Engine;component/Images/Locations/SpiderForest.png");
+            newWorld.AddLocation(0, 1, "Herbalist's hut",
+                "You see a small hut, with plants drying from the roof.",
+                "/Engine;component/Images/Locations/HerbalistsHut.png");
+            newWorld.AddLocation(0, 2, "Herbalist's garden",
+                "There are many plants here, with snakes hiding behind them.",
+                "/Engine;component/Images/Locations/HerbalistsGarden.png");
+
+
+            return newWorld;
+        }
+    }
+}
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,17 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
+using Engine.Factories;
 
 namespace Engine.ViewModels
 {
-    public class GameSession
+    public class GameSession : INotifyPropertyChanged
     {
 
+        private Location _currentLocation;
+        public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
-        public Location CurrentLocation { get; set; }
+        public Location CurrentLocation 
+        {
+            get { return _currentLocation; } 
+            set 
+            {
+                _currentLocation = value;
+                OnPropertyChanged("CurrentLocation");
+                OnPropertyChanged("HasLocationToNorth");
+                OnPropertyChanged("HasLocationToEast");
+                OnPropertyChanged("HasLocationToWest");
+                OnPropertyChanged("HasLocationToSouth");
+            }
+        }
+        public bool HasLocationToNorth
+        {
+            get 
+            { 
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null; 
+            }
+        }
+        public bool HasLocationToEast
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+        public bool HasLocationToSouth
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null;
+            }
+        }
+        public bool HasLocationToWest
+        {
+            get
+            {
+                return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
 
         public GameSession()
         {
@@ -23,12 +67,37 @@ namespace Engine.ViewModels
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
 
-            CurrentLocation = new Location();
-            CurrentLocation.Name = "Home";
-            CurrentLocation.XCoordinate = 0;
-            CurrentLocation.YCoordinate = -1;
-            CurrentLocation.Description = "This is your house";
-            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0, -1);
+        }
+
+        public void MoveNorth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+        }
+
+        public void MoveWest()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+        }
+
+        public void MoveEast()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+        }
+
+        public void MoveSouth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
