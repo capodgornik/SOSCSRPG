f6d2cf0c77139a45af734f59780d434f7e4c1f2d
capod <capod@LAPTOP-DFP4L1CN>
2022-07-21 20:04:56 -0700

    Add game session class; make player class public

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    internal class Player
+    public class Player
     {
-        string Name { get; set; }
-        string CharacterClass { get; set; }
-        int HitPoints { get; set; }
-        int ExperiencePoints { get; set; }
-        int Level { get; set; }
-        int Gold { get; set; }
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
 
     }
 }
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    internal class GameSession
+    {
+
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Celine";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
