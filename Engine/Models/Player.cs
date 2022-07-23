d3e6a3ff35f8a6c7332a0475fe61a7666cb2d644
capod <capod@LAPTOP-DFP4L1CN>
2022-07-23 13:04:58 -0700

    Create player inventory

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -69,6 +70,13 @@ namespace Engine.Models
                 _gold = value;
                 OnPropertyChanged(nameof(Gold));
             }
+        }
+
+        public ObservableCollection<GameItem> Inventory { get; set; }
+
+        public Player()
+        {
+            Inventory = new ObservableCollection<GameItem>();
         }
 
     }
