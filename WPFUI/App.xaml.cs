e6aa2cc2cbfea576cb92875e7126a5d0ece61269
capod <capod@LAPTOP-DFP4L1CN>
2022-08-08 14:36:38 -0700

    Catch and log exceptions

--- WPFUI/App.xaml.cs
+++ WPFUI/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using Engine.Services;
 
 namespace WPFUI
 {
@@ -13,5 +15,15 @@ namespace WPFUI
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string exceptionMessageText =
+                $"An exception occurred: {e.Exception.Message}\r\n\r\nat: {e.Exception.StackTrace}";
+
+            LoggingService.Log(e.Exception);
+
+            // TODO: Create a Window to display the exception information.
+            MessageBox.Show(exceptionMessageText, "Unhandled Exception", MessageBoxButton.OK);
+        }
     }
 }
